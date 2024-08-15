3778c0c55a011b06f0470b6909c7e9a3f27004ff
Caetano Labaki <caetanolz@hotmail.com>
2024-08-15 17:04:24 -0300

    Projeto finalizado

---
+++ src/interfaces.ts
@@ -0,0 +1,20 @@
+export interface IProduct {
+    id: number,
+    name: string,
+    price: number, 
+    createdAt: Date,
+    upDateAt: Date,
+}
+
+export interface IProductService {
+    createProduct(data: { name: string, price: number }): IProduct;
+    getProducts(): IProduct[];
+    getOneProduct(id: number): IProduct | undefined;
+    upDateProduct(id: number, data: { name?: string, price?: number}): IProduct;
+    deleteProduct(id: number): { message: string },
+}
+
+
+
+
+
