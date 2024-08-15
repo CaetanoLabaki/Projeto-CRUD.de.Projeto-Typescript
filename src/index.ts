54478012fa84d552c5b5b228824eea992f74511d
Caetano Labaki <caetanolz@hotmail.com>
2024-08-15 17:06:22 -0300

    Projeto finalizado

---
+++ src/index.ts
@@ -0,0 +1,53 @@
+import { IProduct, IProductService } from "./interfaces";
+
+class ProductList implements IProductService {
+    private productList: IProduct[] = [];
+    currentId: number = 1;
+
+    createProduct(data: {name: string, price: number}): IProduct {
+        const currentDate = new Date();
+        const newProduct: IProduct = {
+            id: this.currentId,
+            name: data.name,
+            price: data.price,
+            createdAt: currentDate,
+            upDateAt: currentDate,  
+        };
+    this.productList.push(newProduct)
+    this.currentId++;
+    return newProduct;   
+    }
+
+    getProducts(): IProduct[] {
+        return this.productList;        
+    }
+
+    getOneProduct(id: number): IProduct | undefined {
+        return this.productList.find(product => product.id === id)
+    }
+
+    upDateProduct(id: number, data: { name?: string; price?: number; }): IProduct {
+        const product = this.productList.find(product => product.id === id);
+        if (!product) {
+            throw new Error('Product not found');
+          }
+          if (data.name !== undefined) {
+            product.name = data.name;
+          }
+          if (data.price !== undefined) {
+            product.price = data.price;
+          }
+          product.upDateAt = new Date();
+          return product;
+    }
+
+    deleteProduct(id: number): { message: string; } {
+        const productIndex = this.productList.findIndex(product => product.id === id);
+        this.productList.splice(productIndex, 1);
+        return { message: "Product successfully deleted."}
+    }    
+}
+
+const productList = new ProductList();
+
+export { productList };
